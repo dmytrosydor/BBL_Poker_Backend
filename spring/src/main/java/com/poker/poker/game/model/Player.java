38c1c6484ff4b660cbee829c4053bfd95a20abe8
dmytrosydor <sydordmytro3@gmail.com>
2025-01-19 14:05:42 +0200

    Developed the logic to handle player actions, such as placing bets, folding, and requesting cards.

--- spring/src/main/java/com/poker/poker/game/model/Player.java
+++ spring/src/main/java/com/poker/poker/game/model/Player.java
@@ -1,14 +1,20 @@
 package com.poker.poker.game.model;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 public class Player {
     private UUID id;
     private String nickname;
+    private int balance; // Баланс гравця
+    private List<Card> hand; // Карти, які має гравець
 
-    public Player(UUID id, String nickname) {
+    public Player(UUID id, String nickname, int balance) {
         this.id = id;
         this.nickname = nickname;
+        this.balance = balance;
+        this.hand = new ArrayList<>();
     }
 
     public UUID getId() {
@@ -25,5 +31,21 @@ public class Player {
 
     public void setNickname(String nickname) {
         this.nickname = nickname;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public List<Card> getHand() {
+        return hand;
+    }
+
+    public void addCards(List<Card> cards) {
+        this.hand.addAll(cards);
     }
 }
---
+++ spring/src/main/java/com/poker/poker/game/model/PlayerAction.java
@@ -0,0 +1,7 @@
+package com.poker.poker.game.model;
+
+public enum PlayerAction {
+    PLACE_BET,
+    FOLD,
+    DRAW_CARD
+}
