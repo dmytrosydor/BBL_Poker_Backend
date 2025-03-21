c33beea723927d63e271129c37eb9cd100ad1d1b
PavloYaremchuk <pavlo.yaremchuk.shi.2023@lpnu.ua>
2025-03-22 01:31:53 +0200

    implementation of the mechanism that stores all the messages written in a specific lobby

--- spring/src/main/java/com/poker/poker/chat/message/ChatController.java
+++ spring/src/main/java/com/poker/poker/chat/message/ChatController.java
@@ -3,14 +3,22 @@ package com.poker.poker.chat.message;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import java.util.UUID;
 
 @Controller
 public class ChatController {
 
-    @MessageMapping("/sendMessage")
-    @SendTo("/topic/messages")
-    public Message send(Message message) {
+    private final ChatMessageService chatMessageService;
+
+    public ChatController(ChatMessageService chatMessageService) {
+        this.chatMessageService = chatMessageService;
+    }
+
+    @MessageMapping("/sendMessage/{lobbyId}")
+    @SendTo("/topic/messages/{lobbyId}")
+    public Message send(Message message, @org.springframework.messaging.handler.annotation.DestinationVariable UUID lobbyId) {
         System.out.println("Received message: " + message.getFrom() + " - " + message.getContent());
+        chatMessageService.addMessage(lobbyId, message);
         return message;
     }
 
